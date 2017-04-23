c014fb967651e5ccc3aefd1df90e62fbdc44e463
Callum Kirkwood <ckirkwood@users.noreply.github.com>
2017-04-23 10:11:16 +0100

    Update dweet_pots.py

--- dweet_pots.py
+++ dweet_pots.py
@@ -1,4 +1,21 @@
-## Send output from pots to Dweet.io
+## Send analogue values to Dweet.io
+
+################################################################################    
+#    Copyright © 2017 Callum Kirkwood                                          #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+################################################################################
 
 import dweepy
 import Adafruit_MCP3008
