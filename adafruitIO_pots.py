ffe41441ec0f38506ba22cbf4c89d7829cc3c03f
Callum Kirkwood <ckirkwood@users.noreply.github.com>
2017-04-23 09:53:06 +0100

    Update adafruitIO_pots.py

--- adafruitIO_pots.py
+++ adafruitIO_pots.py
@@ -8,7 +8,7 @@ import Adafruit_MCP3008
 from time import sleep
 
 # Set to your Adafruit IO key.
-ADAFRUIT_IO_KEY = 'b4b75679a5764fac994eb084d73d4990'
+ADAFRUIT_IO_KEY = 'API_KEY'
 
 # Create an instance of the REST client.
 aio = Client(ADAFRUIT_IO_KEY)
@@ -20,8 +20,8 @@ MOSI = 10
 CS = 8
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
+## Adjust the output range if necessary and send the values to an Adafruit IO Feed
 ## NewValue = (((OldValue - OldMin) * (NewMax - NewMin) / (OldMax - OldMin)) + $
-
 while True:
         pot1 = (((mcp.read_adc(0) - 0) * (255 - 0)) / (1023 - 0)) + 0
         pot2 = (((mcp.read_adc(2) - 0) * (255 - 0)) / (1023 - 0)) + 0
