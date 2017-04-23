f506f933b32ff78ac6894498547dae67617694b6
Callum Kirkwood <ckirkwood@users.noreply.github.com>
2017-04-23 10:10:22 +0100

    Update blinkt_scanner.py

--- blinkt_scanner.py
+++ blinkt_scanner.py
@@ -1,5 +1,22 @@
 ## 1 pot controls the position of the 'on' LED, the other 2 control red and green values; potential use in a UI/menu selector
 
+################################################################################    
+#    Copyright © 2017 Callum Kirkwood                                          #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+################################################################################
+
 import Adafruit_MCP3008
 import blinkt
 import time
