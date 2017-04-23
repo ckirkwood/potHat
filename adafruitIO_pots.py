451b8f0e928cf3fa6037c2d4b9773764b8c86f0e
Callum Kirkwood <ckirkwood@users.noreply.github.com>
2017-04-23 10:09:30 +0100

    Update adafruitIO_pots.py

--- adafruitIO_pots.py
+++ adafruitIO_pots.py
@@ -1,7 +1,22 @@
-# Simple example of sending and receiving values from Adafruit IO with the REST
-# API client.
-# Author: Tony DiCola
+## Publish analogue values to Adafruit IO
+## Adapted from https://github.com/adafruit/Adafruit_Python_MCP3008/blob/master/examples/simpletest.py
 
+################################################################################    
+#    Copyright © 2017 Callum Kirkwood                                          #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+################################################################################
 # Import Adafruit IO REST client.
 from Adafruit_IO import Client
 import Adafruit_MCP3008
